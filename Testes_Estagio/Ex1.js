402ad81ea43f0b7b8e390e1e2ec72e6fa246ac0f
Daniel Silveira <murtdaniel@gmail.com>
2023-03-15 17:21:56 -0300

    Exercicio 1

---
+++ Testes_Estagio/Ex1.js
@@ -0,0 +1,14 @@
+function CalcularSoma() {
+    let indice = 13
+    let soma = 0
+    let K = 0
+
+    while (K < indice){
+        K = K + 1
+        soma = soma + K
+    }
+
+    return soma
+}
+
+console.log(CalcularSoma())
